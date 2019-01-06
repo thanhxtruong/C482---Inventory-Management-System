2f25485d09d0923cb4a7a048e5739b211c82f0de
Thanh Truong <thanhgvsu@gmail.com>
2019-01-06 07:47:31 -0500

    Add code to implement "Add/Delete/Modify/Search Product" from Main Screen. Add code to implement Add and Modify Product screens.

--- src/thanhtruong/MainApp.java
+++ src/thanhtruong/MainApp.java
@@ -12,12 +12,14 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import thanhtruong.model.InhousePart;
 import thanhtruong.model.Inventory;
 import thanhtruong.model.Part;
+import thanhtruong.model.Product;
 import thanhtruong.view_controller.AddPartController;
+import thanhtruong.view_controller.AddProductController;
 import thanhtruong.view_controller.MainScreenController;
 import thanhtruong.view_controller.ModifyPartController;
+import thanhtruong.view_controller.ModifyProductController;
 
 /**
  *
@@ -110,6 +112,56 @@ public class MainApp extends Application {
     }
     
     /**
+     * Called from handleAddProduct() in MainScreenController.
+     * End with showAndWait and pass it to AddProductController.
+     * 
+     * FXMLLoader is used to load the fxml GUI and set up the scene and stage
+     * initModality() and initOwner() methods are used to display the AddPart screen
+     * on top of the MainScreen (parent-child relationship) and prevent the user from
+     * closing the MainScreen until the AddPart screen is closed.
+     * 
+     * getController() for the fxml in order to pass mainApp, addProductStage, and call setProductID()
+     * Controller can then access methods and fields in mainApp and close the dialog/stage
+     * 
+     * @return isSaveClicked: true/false passed to controller
+     */
+    public boolean showAddProductDialog(){
+        
+        try {
+            // Load the fxml file and create a new stage for the popup dialog
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view_controller/AddProduct.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            
+            // Create the dialog Stage
+            Stage addProductStage = new Stage();
+            addProductStage.setTitle("Inventory MS - Add Product");
+            addProductStage.initModality(Modality.WINDOW_MODAL);
+            addProductStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            addProductStage.setScene(scene);
+            
+            // Set the controller in order to pass the stage and call setProductID()
+            AddProductController controller = loader.getController();
+            controller.setMainApp(this);
+            controller.setDialogStage(addProductStage);
+            controller.setProductID();            
+            controller.addProductDialogDisplay();
+            
+            addProductStage.showAndWait();
+            
+            // Return "saveClicked" from AddProductController as soon as user clickes "Save"
+            return controller.isSaveClicked();
+            
+        } catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    
+    
+    /**
      * Called from handleModifyPart() in MainScreenController
      * End with showAndWait and pass it to ModifyPartController
      * @return isSaveClicked
@@ -139,6 +191,46 @@ public class MainApp extends Application {
             modifyPartStage.showAndWait();
             
             // Return "saveClicked" from AddPartController as soon as user clickes "Save"
+            return controller.isSaveClicked();
+            
+        } catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    /**
+     * Called from handleModifyPart() in MainScreenController
+     * End with showAndWait and pass it to ModifyPartController
+     * @return isSaveClicked
+     */
+    public boolean showModifyProductDialog(Product product){
+        
+        try {
+            // Load the fxml file and create a new stage for the popup dialog
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view_controller/ModifyProduct.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            
+            // Create the dialog Stage
+            Stage modifyProductStage = new Stage();
+            modifyProductStage.setTitle("Inventory MS - Modify Product");
+            modifyProductStage.initModality(Modality.WINDOW_MODAL);
+            modifyProductStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            modifyProductStage.setScene(scene);
+            
+            // Set the controller in order to pass the stage and call setProductID()
+            // Pass and set selected product in controller
+            ModifyProductController controller = loader.getController();
+            controller.setMainApp(this);
+            controller.setProduct(product);
+            controller.setDialogStage(modifyProductStage);
+            controller.modifyProductDialogDisplay(product);
+            
+            modifyProductStage.showAndWait();
+            
+            // Return "saveClicked" from AddProductController as soon as user clickes "Save"
             return controller.isSaveClicked();
             
         } catch(IOException e){
